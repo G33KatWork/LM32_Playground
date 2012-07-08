c789d30228f124d569f015584c8f0501a5bba9cb
Andreas Galauner <andreas@galauner.de>
2012-07-08 04:40:17 +0200

    boot-dump-memory for testing added

---
+++ firmware/boot-dump-memory/main.c
@@ -0,0 +1,61 @@
+/**
+ * Memory dumper
+ */
+#include "soc-hw.h"
+
+#define     TEST_SIZE           0x10
+#define     TEST_STARTADDRESS   0x40000001
+
+const char hexchars[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+void putHex(uint32_t i)
+{
+    uart_putchar(hexchars[(i >> 28) & 0x0F]);
+    uart_putchar(hexchars[(i >> 24) & 0x0F]);
+    uart_putchar(hexchars[(i >> 20) & 0x0F]);
+    uart_putchar(hexchars[(i >> 16) & 0x0F]);
+    uart_putchar(hexchars[(i >> 12) & 0x0F]);
+    uart_putchar(hexchars[(i >> 8) & 0x0F]);
+    uart_putchar(hexchars[(i >> 4) & 0x0F]);
+    uart_putchar(hexchars[i & 0x0F]);
+}
+
+int main(int argc, char **argv)
+{
+    // Initialize UART
+    uart_init();
+
+    uart_putstr("Dumping memory...\r\n");
+    uint32_t* q = (uint32_t*)TEST_STARTADDRESS;
+    for(int i = 0; i < TEST_SIZE/4; i++)
+    {   
+        putHex(*q);
+        q++;
+        uart_putstr("\r\n");
+    }
+    uart_putstr("\r\ndone!\r\n");
+}
+
+/*void putHex(uint8_t i)
+{
+    uart_putchar(hexchars[i >> 4]);
+    uart_putchar(hexchars[i & 0x0F]);
+}
+
+int main(int argc, char **argv)
+{
+    // Initialize UART
+	uart_init();
+
+	uart_putstr("Dumping memory...\r\n");
+	uint8_t* q = (uint8_t*)TEST_STARTADDRESS;
+    for(int i = 0; i < TEST_SIZE; i++)
+    {
+        if(i % 4 == 0)
+            uart_putstr("\r\n");
+        
+        putHex(*q);
+        q++;
+    }
+    uart_putstr("\r\ndone!\r\n");
+}*/
